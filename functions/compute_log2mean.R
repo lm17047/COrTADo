a90ff991985008efc9f0c85aaec9780649ab884d
Liudmila Mikheeva <88399120+lm17047@users.noreply.github.com>
2022-03-07 12:40:37 +0300

    Update compute_log2mean.R

--- functions/compute_log2mean.R
+++ functions/compute_log2mean.R
@@ -44,3 +44,32 @@ compute_log2mean <- function(data_list, replace_zero = FALSE, window_size, cores
 # Return the resulting matrix                                        
         return(result)
 }
+
+# Compute log2 mean ratio at column position x based on the interactions within the given Hi-C row/column
+.estimate_ma_mean <- function(x, data_set, data_index, window_size, start_limit, end_limit){
+        if (!is.na(x)){
+                if (x <= start_limit){result <- NaN}
+                if (x >= end_limit){result <- NaN}
+                if (x > start_limit & x < end_limit){
+                        mean_left <- .estimate_single_ma_mean(
+                                data_set = data_set, 
+                                data_index = data_index,
+                                x_start = x - window_size, 
+                                x_end = x - 1)
+                        mean_right <- .estimate_single_ma_mean(
+                                data_set = data_set, 
+                                data_index = data_index,
+                                x_start = x + 1, 
+                                x_end = x + window_size)
+                        if (is.na(mean_left) | is.na(mean_right)){result <- NA}
+                        if (!is.na(mean_left) & !is.na(mean_right)){result <- log2(mean_right/mean_left)}}
+        } else {result <- NA}
+        return(result)
+}
+
+# Compute MA mean estimate from provided interval based on the interactions within the given Hi-C row/column
+.estimate_single_ma_mean <- function(data_set, data_index, x_start, x_end){
+        select_set <- data_set[match(c(x_start:x_end), data_index)]
+        result <- mean(select_set, na.rm = TRUE)
+        return(result)
+}
